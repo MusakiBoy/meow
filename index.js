988c93a6679e3fe3d1336010a3a1f3cf5b429b09
MusakiBoy <134459998+MusakiBoy@users.noreply.github.com>
2023-05-23 19:58:56 -0500

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-alert('Sabia que ibas a decir que si uwu, sin embargo no esta mal que me lo recuerdes de vez en cuando, a su vez debo decirte que yo te amo con todo lo que conlleva tu ser querida muah muah <3')
+alert('Sabia que ibas a decir que si uwu, sin embargo no esta mal que me lo recuerdes de vez en cuando, a su vez debo decirte que yo te amo con todo lo que conlleva tu ser querida, siempre estare apoyandote y para morderte jejejje (se que debo morder menos perdon) y teta porfavor muah muah <3')
 });
 
 const noBtn = document.querySelector('#noBtn');
